dd46f9dc7fb874d91992f82f8178a4f8e9c2a730
ecomloop <andrew@ecomloop.com>
2020-03-24 17:05:23 -0700

    add accordion

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,17 +4,17 @@ import './Footer.css'
 
 export default () => (
   <div>
-    <h2 className="taCenter">
+    <!--<h2 className="taCenter">
       Follow us{' '}
       <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
     </h2>
     <br />
-    <InstagramFeed count="8" />
+    <InstagramFeed count="8" />--
     <footer className="footer">
       <div className="container taCenter">
         <span>
           © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          <a href="https://ecomloop.com">ecomloop</a>.
         </span>
       </div>
     </footer>
