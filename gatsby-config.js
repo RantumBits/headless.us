d2a3e1ca5bb2b30f3260913d9d5d33b5ba0f1168
ecomloop <andrew@ecomloop.com>
2020-03-31 16:06:39 -0700

    add shared links

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,13 @@ module.exports = {
     siteUrl: 'https://ecomloop.com'
   },
   plugins: [
+    'gatsby-source-google-docs',
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: ["gatsby-remark-images"],
+      },
+    },
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-yaml',
     {
