9f94648e8f8ddbc4085a846bcddd14c7914e0ba1
ecomloop <andrew@ecomloop.com>
2020-04-06 17:02:23 -0700

    recpatcha sitekey update

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -153,7 +153,7 @@ class Form extends React.Component {
           </label>
           <div
             className="g-recaptcha"
-            data-sitekey="6LfKN3kUAAAAAGIM1CbXmaRZx3LIh_W2twn1tzkA"
+            data-sitekey="6Ldul-YUAAAAALyKSfu7gwZOQV2eh2E7g9PnmfTT"
           />
           {!!subject && <input type="hidden" name="subject" value={subject} />}
           <input type="hidden" name="form-name" value={name} />
