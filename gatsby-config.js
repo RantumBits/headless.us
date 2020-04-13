ecb0dd75ce7377f4ad4aaedcc29fa9c41f0955a9
ecomloop <andrew@ecomloop.com>
2020-04-13 12:52:16 -0700

    add images

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,7 +71,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/images`,
+        path: `${__dirname}/static/images`,
         name: 'images'
       }
     },
