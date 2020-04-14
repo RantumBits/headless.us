352d5c658364f012c8b40675b8aec2c8a9a63f01
ecomloop <andrew@ecomloop.com>
2020-04-14 11:06:35 -0700

    fix import config

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,14 +15,18 @@ module.exports = {
       destination: `${__dirname}/content/posts`,
       exportGDocs: false,
       exportMimeType: ''
+    }
     },
+    'gatsby-plugin-react-helmet',
+    {
+    resolve: '@fs/gatsby-plugin-drive',
     options: {
       folderId: '1ZUcbDFOxg7UEjyy0f-xmqf7ZGgUkM4rE',
       keyFile: `${__dirname}/client_secret.json`,
       destination: `${__dirname}/static/images`,
       exportGDocs: false,
       exportMimeType: ''
-    }
+      }
     },
     'gatsby-transformer-yaml',
 
