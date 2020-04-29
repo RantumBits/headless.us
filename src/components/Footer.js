5632dac42978af4c16ce6d150e6f19cf838b1bcb
ecomloop <andrew@ecomloop.com>
2020-04-29 16:57:20 -0700

    add projects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,11 @@ import './Footer.css'
 
 export default () => (
   <div>
-    
+
     <footer className="footer">
       <div className="container taCenter">
         <span>
+          <a href="https://twitter.com/ecomloop">twitter</a> <a href="https://github.com/ecomloop">github</a> <a href="https://www.upwork.com/agencies/~018dcd9aca877342a3">upwork</a> <a href="https://www.linkedin.com/company/ecomloop/">linkedin</a>
           © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
           <a href="https://ecomloop.com">ecomloop</a>.
         </span>
--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -13,7 +13,7 @@ class GoogleMap extends Component {
       lat: 37.5780511,
       lng: -122.3510674
     },
-    zoom: 14
+    zoom: 16
   }
 
   render() {
@@ -25,7 +25,7 @@ class GoogleMap extends Component {
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          <Marker lat={-28.0914483} lng={153.4425208} text={'Kreyser Avrora'} />
+          <Marker lat={37.5780511} lng={-122.3510674} text={'here we are!'} />
         </GoogleMapReact>
       </div>
     )
@@ -36,7 +36,7 @@ export default GoogleMap
 
 const Marker = () => {
   return (
-    <div style={{ color: 'red' }}>
+    <div style={{ color: 'blue' }}>
       <MapPin />
     </div>
   )
