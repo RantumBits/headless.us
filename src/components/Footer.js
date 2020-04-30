636dccf06690316bf3940eab2088474466535606
ecomloop <andrew@ecomloop.com>
2020-04-29 17:28:17 -0700

    update footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ export default () => (
     <footer className="footer">
       <div className="container taCenter">
         <span>
-          <a href="https://twitter.com/ecomloop">twitter</a> <a href="https://github.com/ecomloop">github</a> <a href="https://www.upwork.com/agencies/~018dcd9aca877342a3">upwork</a> <a href="https://www.linkedin.com/company/ecomloop/">linkedin</a>
+          <a href="https://twitter.com/ecomloop" target="_blank">twitter</a> <a href="https://github.com/ecomloop" target="_blank">github</a> <a href="https://www.upwork.com/agencies/~018dcd9aca877342a3" target="_blank">upwork</a> <a target="_blank" href="https://www.linkedin.com/company/ecomloop/">linkedin</a>
           <br/>© Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
           <a href="https://ecomloop.com">ecomloop</a>.
         </span>
