8224ac1cab2859cdda4b1eb3a37f683c73206688
ecomloop <andrew@ecomloop.com>
2020-05-13 13:47:29 -0700

    init

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ export default () => (
     <footer className="footer">
       <div className="container taCenter">
         <span>
-          <a href="/contact/">contact</a> <a href="https://twitter.com/ecomloop" target="_blank">twitter</a> <a href="https://github.com/ecomloop" target="_blank">github</a> <a href="https://www.upwork.com/agencies/~018dcd9aca877342a3" target="_blank">upwork</a> <a target="_blank" href="https://www.linkedin.com/company/ecomloop/">linkedin</a>
+          <a href="/contact/">contact</a> <a href="https://twitter.com/ecomloop" target="_blank"  rel="noopener noreferrer">twitter</a> <a href="https://github.com/ecomloop" target="_blank"  rel="noopener noreferrer">github</a> <a href="https://www.upwork.com/agencies/~018dcd9aca877342a3" target="_blank" rel="noopener noreferrer">upwork</a> <a  rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/company/ecomloop/">linkedin</a>
           <br/>© Copyright {new Date().getFullYear()} All rights reserved.
         </span>
       </div>
